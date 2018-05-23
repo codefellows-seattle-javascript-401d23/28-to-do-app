2e31afcc693ae23aa17eeb35c3bd20135d8e09e0
dahby <dstoll243@gmail.com>
2018-05-23 13:55:38 -0700

    build out webpack config files, main.js, autoBind helper function, & app component

--- src/component/app.js
+++ src/component/app.js
@@ -1,1 +1,36 @@
-'use strict';
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Dashboard from './dashboard';
+import '../styles/main.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className = 'app'>
+        <BrowserRouter>
+          <div>
+            <header>
+              <h1>To-Do App</h1>
+              <nav>
+                <ul>
+                  <li><Link to = '/'>Landing</Link></li>
+                  <li><Link to = '/dashboard'>Dashboard</Link></li>
+                </ul>
+              </nav>
+            </header>
+            <Route
+              exact
+              path = '/'
+              component = { () => <h2>List what you need to do, and then do that.</h2>}
+            />
+            <Route
+              exact
+              path = '/dashboard'
+              component = {Dashboard}
+            />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,8 @@
-'use strict';
+export default function autoBind(classComponent) {
+  const classMethods = Object.getOwnPropertyNames(classComponent.prototype);
+  classMethods.forEach((method) => {
+    if (method.startsWith('handle')) {
+      this[method] = this[method].bind(this);
+    }
+  });
+}
