56af6d4b1b7a1a95f943ec0d203f9bdbea6b6866
Joanna Coll <johannam999@gmail.com>
2018-05-23 22:37:48 -0700

    comments removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,7 +2,7 @@ export default function autoBind(classComponent) {
   const classMethods = Object.getOwnPropertyNames(classComponent.prototype);
   classMethods.forEach((method) => {
     if (method.startsWith('handle')) {
-      this[method] = this[method].bind(this); // its same as this.method but it might be long string
+      this[method] = this[method].bind(this); 
     }
   });
 }
