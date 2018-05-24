29e79b0c099af8020c141f497676e1d92a1ac70a
Joanna Coll <johannam999@gmail.com>
2018-05-23 22:36:39 -0700

    css changes

--- src/component/note-item/index.js
+++ src/component/note-item/index.js
@@ -1,22 +1,13 @@
 import React from 'react';
 
 export default class NoteItem extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.handleRemove = this.handleRemove.bind(this);
-  // }
-  // handleRemove(event) {
-  //   console.log(this.props.note.id);
-  //   event.preventDefault();
-  //   this.props.handleRemoveItem(this.props.note.id);
-  // }
   render() {
     return (
       <section>
-        <li key={this.props.note.id}>
-          "{this.props.note.title}" : {this.props.note.content}
+        <li className='title' key={this.props.note.id}>
+        {this.props.note.title} - {this.props.note.content}
         </li>
-        <button onClick={
+        <button className='remove' onClick={
           () => {
             return this.props.handleRemoveItem(this.props.note.id);
           }
