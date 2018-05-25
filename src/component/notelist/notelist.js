a8e57761e2dd995b84d3cfcf8014cd4bff54b3d4
Ryan Groesch <ryan.groesch@gmail.com>
2018-05-25 08:50:31 -0700

    Lab submission

--- src/component/notelist/notelist.js
+++ src/component/notelist/notelist.js
@@ -3,7 +3,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import NoteItem from '../noteitem/noteitem';
-import '../../styles/main.scss';
 import autoBind from '../../utils';
 
 export default class NoteList extends React.Component {
